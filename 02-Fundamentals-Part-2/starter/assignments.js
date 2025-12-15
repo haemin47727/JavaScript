8a76789b647d39401a17eafc4fce02c195fd4b74
haemin47727 <sginstockx@gmail.com>
2025-12-14 21:39:25 -0500

    commit

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,13 +1,62 @@
 "use strict";
 
-function describeCountry(country, population, capitalCity) {
-  const describe = `${country} has ${population} million people and its capital city is ${capitalCity}`;
-  return describe;
-}
+// function describeCountry(country, population, capitalCity) {
+//   const describe = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//   return describe;
+// }
 
-const korea = describeCountry("Korea", 55, "Seoul");
-console.log(korea);
-const finland = describeCountry("Finland", 6, "Helsinki");
-console.log(finland);
-const london = describeCountry("England", 57, "London");
-console.log(london);
+// const korea = describeCountry("Korea", 55, "Seoul");
+// (korea);
+// const finland = describeCountry("Finland", 6, "Helsinki");
+// (finland);
+// const london = describeCountry("England", 57, "London");
+// (london);
+
+// function percentageOfWorld1(population) {
+//   return population / 7900 * 100;
+// }
+
+// const average1 = percentageOfWorld1(55);
+// (average1);
+// const average2 = percentageOfWorld1(6);
+// (average2);
+// const average3 = percentageOfWorld1(57);
+// (average3);
+
+// const percentageOfWorld2 = function (population) {
+//   return population / 7900 * 100;
+// }
+// const percentageOfWorld3 = population => population / 7900 * 100;
+
+// const describePopulation = (country, population) => {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million people, which is about ${percentage} of the world`;
+//   (description);
+// }
+// describePopulation("Korea", 50);
+
+// const populations = [50, 5, 15, 100];
+// if (populations.length === 4) {
+//   console.log("The array has 4 elements");
+// }
+// const percentageOfWorld1 = population => population / 7900 * 100;
+// const percentage = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3])
+// ];
+// console.log(percentage);
+
+// const neighbors = ["China", "Japan"];
+// neighbors.push("Utopia");
+// console.log(neighbors);
+// neighbors.pop();
+// console.log(neighbors);
+// if (!neighbors.includes("Germany")) {
+//   console.log("Probably not a central european country :D");
+// }
+// neighbors[neighbors.indexOf("China")] = "Singapore";
+// console.log(neighbors);
+
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,18 +10,95 @@
 // if (hasDriversLicense) console.log("I can drive :D");
 
 //function//
-function logger() {
-  console.log("My name is Julia.");
-}
-logger(); //calling or running function
+// function logger() {
+//   console.log("My name is Julia.");
+// }
+// logger(); //calling or running function
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// //function declaration//
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+//function expression//
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
+
+//arrow function//
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years.`;
+// }
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years.`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired.`);
+//     return -1;
+//   }
+// }
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+//arrays//
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+
+// const years = new Array(1994, 1984, 2008, 2020);
+// console.log(friends[0]);
+// console.log(friends[friends.length - 1]);
+// const newLength = friends.push("Jay"); //add to end//
+// console.log(`pushed ${newLength}`);
+// console.log(friends);
+// friends.unshift("John");
+// console.log(friends);
+
+// friends.pop(); // remove last element
+// const popped = friends.pop();
+// console.log(`popped ${popped}`);
+// console.log(friends);
+// friends.shift(); // remove first
+// console.log(friends);
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.includes("Steven"));
